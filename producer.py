2c75b48ff6a0622b949d9125aad0080daf8258c6
Yasmin Feitosa <yasminfeitosa@usp.br>
2021-05-21 16:02:17 -0300

    [feat] Adding producer

---
+++ producer.py
@@ -0,0 +1,23 @@
+import boto3
+
+sqs = boto3.resource('sqs')
+
+def sendMessage(queueName, message, autor):
+    queue = sqs.get_queue_by_name(QueueName=queueName)
+
+    response = queue.send_message(MessageBody=message, MessageAttributes={
+        'Author': {
+            'StringValue': autor,
+            'DataType': 'String'
+        }
+    })
+
+    print('MessageId: ',response.get('MessageId'))
+    print('MD5OfMessageBody: ',response.get('MD5OfMessageBody'))
+
+    try:
+        return response.get('MessageId')
+    except: 
+        return 'Erro: Não foi possível inserir a mensagem na fila'
+    
+sendMessage('hackday_test', 'Apagar', 'Yas')
